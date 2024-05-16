65632eb7073604ee876887fec94953f218dae573
MelinaClouet <melina.clouet@my-digital-school.org>
2024-05-16 17:16:31 +0200

    [ADD] inscription

--- lib/screen.register.dart
+++ lib/screen.register.dart
@@ -1,4 +1,13 @@
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_api/screen.home.dart';
+import 'package:flutter_api/screen.login.dart';
+
+import 'package:http/http.dart' as http;
+
 
 class ScreenRegister extends StatefulWidget {
   const ScreenRegister({Key? key});
@@ -10,6 +19,11 @@ class ScreenRegister extends StatefulWidget {
 class _ScreenRegisterState extends State<ScreenRegister> {
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController nomController = TextEditingController();
+  TextEditingController prenomController = TextEditingController();
+  TextEditingController pseudoController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+
 
   bool passwordsMatch = true;
 
@@ -22,6 +36,10 @@ class _ScreenRegisterState extends State<ScreenRegister> {
   @override
   void dispose() {
     passwordController.dispose();
+    nomController.dispose();
+    prenomController.dispose();
+    pseudoController.dispose();
+    mailController.dispose();
     confirmPasswordController.removeListener(updatePasswordMatch);
     confirmPasswordController.dispose();
     super.dispose();
@@ -56,37 +74,41 @@ class _ScreenRegisterState extends State<ScreenRegister> {
               children: [
                 Column(
                   children: [
-                    const Padding(
+                     Padding(
                       padding: EdgeInsets.all(8.0),
                       child: TextField(
+                        controller: nomController, // Ajout du contrôleur ici
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: 'Nom',
                         ),
                       ),
                     ),
-                    const Padding(
+
+                    Padding(
                       padding: EdgeInsets.all(8.0),
                       child: TextField(
-
+                        controller: prenomController, // Ajout du contrôleur ici
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: 'Prénom',
                         ),
                       ),
                     ),
-                    const Padding(
+                    Padding(
                       padding: EdgeInsets.all(8.0),
                       child: TextField(
+                        controller: pseudoController, // Ajout du contrôleur ici
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: 'Pseudo',
                         ),
                       ),
                     ),
-                    const Padding(
+                    Padding(
                       padding: EdgeInsets.all(8.0),
                       child: TextField(
+                        controller: mailController, // Ajout du contrôleur ici
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: 'Mail',
@@ -122,29 +144,78 @@ class _ScreenRegisterState extends State<ScreenRegister> {
                   height: 90,
                 ),
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (passwordsMatch) {
 
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => ScreenRegister()),
-                      );
+                      // Effectuer la requête HTTP
+                      var url = Uri.parse('https://mds.sprw.dev/users');
+                      http.post(url, body: jsonEncode({
+                        'username': pseudoController.text,
+                        'password': passwordController.text,
+                        'email': mailController.text,
+                        'firstname': prenomController.text,
+                        'lastname': nomController.text,
 
+                      })).then((response) {
+                        debugPrint(response.statusCode.toString());
+                        if(response.statusCode == 201){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ScreenHome(),
+                              settings: RouteSettings(
+                                arguments: response.body,
+                              ),
+                              // Pass the arguments as part of the RouteSettings.
+
+                            ),
+                          );
+                        }
+                        if(response.statusCode==400){
+                          debugPrint("Response body: ${response.body}");
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              backgroundColor: Colors.red,
+                              content: Text('Identifiant ou mot de passe incorrect',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          );
+                        }
+                        else{
+                         debugPrint('else');
+                        }
+                      });
                     } else {
-
-
-
+                      // Afficher un message si les mots de passe ne correspondent pas
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Erreur'),
+                            content: Text('Les mots de passe ne correspondent pas.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     }
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF80586B), // Couleur de fond du bouton
-                    minimumSize: const Size(double.infinity, 50), // Largeur étendue sur toute la largeur de l'écran, hauteur 50
+                    backgroundColor: Color(0xFF80586B),
+                    minimumSize: const Size(double.infinity, 50),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10), // Bordures arrondies
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   child: const Text(
-                    'Se connecter',
+                    'S\'inscrire',
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
